552a57b82b2b01250e927d9da6e7e97497f6d48f
Angelo Gabrieli <agabriel@redhat.com>
2025-06-10 15:27:44 +0200

    Solves typos

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -169,8 +169,7 @@ resource "azurerm_linux_virtual_machine" "jumphost" {
   resource_group_name = azurerm_resource_group.aro_rg.name
   location            = azurerm_resource_group.aro_rg.location
   size                = "Standard_B2s"
-  custom_data         = base64encode(templatefile("./customdata.tpl"))
-  }
+  custom_data         = base64encode(templatefile("./customdata.tpl",{}))
 
   admin_username      = "adminuser"
   network_interface_ids = [

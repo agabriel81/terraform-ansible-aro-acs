1f621544861f7c07029cfaac9040b20e269a1d83
Angelo Gabrieli <agabriel@redhat.com>
2025-06-13 15:52:27 +0200

    Prints ansible output at the end of the playbook

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -285,8 +285,7 @@ resource "null_resource" "ansible_playbook" {
       "ansible-playbook /home/adminuser/terraform-ansible-aro-acs/ansible/playbook.yaml -e ocp4_workload_rhacs_central_admin_password=${var.acs_password}",
       "oc apply -f ~/terraform-ansible-aro-acs/sample_applications/kubernetes-manifests/ --recursive",
       "oc apply -f ~/terraform-ansible-aro-acs/sample_applications/skupper-demo/ --recursive",
-      "oc apply -f ~/terraform-ansible-aro-acs/sample_applications/openshift-pipelines/ --recursive",
-      "ansible-playbook /home/adminuser/terraform-ansible-aro-acs/ansible/playbook.yaml --tags "output"}"
+      "oc apply -f ~/terraform-ansible-aro-acs/sample_applications/openshift-pipelines/ --recursive"
     ]
 
     connection {
